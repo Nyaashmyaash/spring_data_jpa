3b0d6757a2dcf44ec1b22eefd497184fe8130838
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-06-13 08:29:38 +0700

    add changes

--- src/main/java/com/nyash/spring/springboot/spring_data_jpa/controller/EmployeeRESTController.java
+++ src/main/java/com/nyash/spring/springboot/spring_data_jpa/controller/EmployeeRESTController.java
@@ -48,4 +48,10 @@ public class EmployeeRESTController {
         employeeService.deleteEmployee(id);
         return "Employee with ID = " + id + " was deleted!";
     }
+
+    @GetMapping("employees/name/{name}")
+    public List<Employee> showAllEmployeesByName(@PathVariable String name){
+        List<Employee> employees = employeeService.findAllByName(name);
+        return employees;
+    }
 }
